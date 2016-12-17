ffbd2e1be30035cdd921adc8b245a50767bbb474
rstruthers1 <rstruthers1@yahoo.com>
2016-12-17 12:48:23 -0600

    Adding routes, views

--- index.js
+++ index.js
@@ -3,16 +3,59 @@
  */
 
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var index = require('./routes/index');
 
-app.get('/', function(request, response) {
-    response.send('Hello World I\'m connected to github!')
-})
+var app = express();
 
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
-})
+
+app.set('port', (process.env.PORT || 5000));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+// Start server.
+var server = app.listen(5000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('bbstats-node listening at http://%s:%s', host, port);
+});
